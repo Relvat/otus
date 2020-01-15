04cc5bc8d8f36a0daa32df5bf74489ed71315330
Relvat <relvat93@gmail.com>
2020-01-15 22:28:10 +0300

    first commit

---
+++ src/test/java/OpenOtusPage.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+
+@RunWith(Parameterized.class)
+public class OpenOtusPage {
+
+    private static final Logger logger = LogManager.getLogger(OpenOtusPage.class);
+
+    private WebDriver driver;
+
+    @Parameter
+    public Class<? extends WebDriver> driverClass;
+
+    @Parameters(name = "{index}: {0}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] { { ChromeDriver.class },
+                { FirefoxDriver.class } });
+    }
+
+    @Before
+    public void setupTest() throws Exception {
+        WebDriverManager.getInstance(driverClass).setup();
+        driver = driverClass.newInstance();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void test() {
+        WebDriverWait wait = new WebDriverWait(driver, 30);
+        driver.get("https://otus.ru/");
+    }
+}
