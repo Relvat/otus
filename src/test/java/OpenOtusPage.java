46b5f518a4b354dab578be78d782e02855523b62
Relvat <relvat93@gmail.com>
2020-01-20 23:56:59 +0300

    commit

--- src/test/java/OpenOtusPage.java
+++ src/test/java/OpenOtusPage.java
@@ -1,31 +1,56 @@
+import java.util.Arrays;
+import java.util.Collection;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameter;
+import org.junit.runners.Parameterized.Parameters;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import io.github.bonigarcia.wdm.WebDriverManager;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.junit.After;
-import org.junit.BeforeClass;
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
 
 
-public class OpenOtusPage{
+@RunWith(Parameterized.class)
+public class OpenOtusPage {
+
     private static final Logger logger = LogManager.getLogger(OpenOtusPage.class);
-    WebDriver wd = Browsers.CHROME.create();
 
-    @BeforeClass
-    public static void setUpClass() {
-        WebDriverManager.chromedriver().setup();
-        WebDriverManager.firefoxdriver().setup();
+    private WebDriver driver;
+
+    @Parameter
+    public Class<? extends WebDriver> driverClass;
+
+    @Parameters(name = "{index}: {0}")
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][] { { ChromeDriver.class },
+                { FirefoxDriver.class } });
+    }
+
+    @Before
+    public void setupTest() throws Exception {
+        WebDriverManager.getInstance(driverClass).setup();
+        driver = driverClass.newInstance();
     }
 
     @After
-    public void tearDown() {
-        if (wd != null) {
-            wd.quit();
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
         }
     }
 
     @Test
     public void test() {
-        wd.get("https://otus.ru");
+        WebDriverWait wait = new WebDriverWait(driver, 40);
+        driver.get("https://otus.ru/");
     }
 }
